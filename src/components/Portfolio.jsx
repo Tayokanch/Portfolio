487264560136c11bb39749891f04325dacae7632
Tayokanch <tayoquadri11@gmail.com>
2024-06-03 09:52:00 +0100

    i changed the url of the first project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -17,8 +17,60 @@ function Porfolio() {
           <span class="border">My Project</span>
         </div>
       </figure>
-      <section className="portfolio" ref={portfolioRef}>
+
+      <section className="portfolio">
         <h5>Project 1</h5>
+        <div className="project_container">
+          <figure>
+            <img src={trainapp} alt="" />
+          </figure>
+          <div>
+            <h4>Train Ticket Selling App</h4>
+            <p>
+              The proposed web application facilitates the resale of valid train
+              tickets across the UK, providing a platform for sellers to post
+              their tickets for sale. Buyers can access a comprehensive list of
+              available tickets and filter them based on their preferred
+              location. Additionally, buyers can chat with the sellers to
+              inquire about ticket details or initiate the purchase process.
+              This platform serves as a convenient solution for individuals
+              looking to resell their train tickets before their expiration,
+              thereby avoiding potential loss without the option for refunds
+              from the original purchase.
+            </p>
+            <div className="technologies">
+              <p>Tech used:</p>
+              <div>
+                <p>React</p>
+              </div>
+            </div>
+
+            <div className="project_link">
+              <div className="github">
+                <a
+                  href="https://github.com/Tayokanch/Ticket-Traders"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </div>
+
+              <div className="live">
+                <a
+                  href="https://cheapticket.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faGlobe} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="portfolio" ref={portfolioRef}>
+        <h5>Project 2</h5>
         <div className="project_container">
           <figure>
             <img src={explorelag} alt="" />
@@ -58,58 +110,6 @@ function Porfolio() {
               <div className="live">
                 <a
                   href="https://explorelagos.netlify.app/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon icon={faGlobe} />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="portfolio">
-        <h5>Project 2</h5>
-        <div className="project_container">
-          <figure>
-            <img src={trainapp} alt="" />
-          </figure>
-          <div>
-            <h4>Train Ticket Selling App</h4>
-            <p>
-              The proposed web application facilitates the resale of valid train
-              tickets across the UK, providing a platform for sellers to post
-              their tickets for sale. Buyers can access a comprehensive list of
-              available tickets and filter them based on their preferred
-              location. Additionally, buyers can chat with the sellers to
-              inquire about ticket details or initiate the purchase process.
-              This platform serves as a convenient solution for individuals
-              looking to resell their train tickets before their expiration,
-              thereby avoiding potential loss without the option for refunds
-              from the original purchase.
-            </p>
-            <div className="technologies">
-              <p>Tech used:</p>
-              <div>
-                <p>React</p>
-              </div>
-            </div>
-
-            <div className="project_link">
-              <div className="github">
-                <a
-                  href="https://github.com/Tayokanch/Ticket-Traders"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FontAwesomeIcon icon={faGithub} />
-                </a>
-              </div>
-
-              <div className="live">
-                <a
-                  href="https://superlative-eclair-6a2967.netlify.app/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
